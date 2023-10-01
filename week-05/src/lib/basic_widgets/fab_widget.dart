5d4241c8535483f1b62d91a848be08789e8c14da
alfanalivan <alfanalivan@gmail.com>
2023-10-01 18:31:41 +0700

    praktikum-04: flutter bag 1

---
+++ week-05/src/lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyFabWidget extends StatelessWidget {
+  const MyFabWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.thumb_up),
+      ),
+    ));
+  }
+}
