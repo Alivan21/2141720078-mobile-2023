34247801a9b542191c4c3f2838a73a547b1b88ea
alfanalivan <alfanalivan@gmail.com>
2023-10-02 10:11:09 +0700

    week_04: Dokumentasi Updated

---
+++ week-05/README.md
@@ -0,0 +1,66 @@
+## Repository Mobile Programming Class 2023.
+
+Nama : Alfan Olivan
+
+NIM : 2141720078
+
+Tugas : Minggu 5
+
+<div align="center">
+  <h2>Pertemuan Kelima - Flutter Fundamental 1</h2>
+</div>
+
+### Table of Contents
+
+- [Repository Mobile Programming Class 2023.](#repository-mobile-programming-class-2023)
+  - [Table of Contents](#table-of-contents)
+  - [Praktikum 3 - Menerapkan Widget Dasar](#praktikum-3---menerapkan-widget-dasar)
+  - [Praktikum 4 - Menerapkan Widget Material Design \& Cupertino iOS](#praktikum-4---menerapkan-widget-material-design--cupertino-ios)
+
+### Praktikum 3 - Menerapkan Widget Dasar
+
+1. Text Widget
+
+   Output :
+
+   ![Output-1!](/week-05/docs/1.jpg "Output-1")
+
+2. Image Widget
+
+   Output :
+
+   ![Output-2!](/week-05/docs/2.jpg "Output-2")
+
+### Praktikum 4 - Menerapkan Widget Material Design & Cupertino iOS
+
+1. Scaffold Widget
+
+   Output :
+
+   ![Output-3!](/week-05/docs/3.jpg "Output-3")
+
+2. Dialog Widget
+
+   Output :
+
+   ![Output-4!](/week-05/docs/4.jpg "Output-4")
+
+   Output setelah di klik :
+
+   ![Output-5!](/week-05/docs/5.jpg "Output-5")
+
+3. Input Widget
+
+   Output :
+
+   ![Output-6!](/week-05/docs/6.jpg "Output-6")
+
+4. Date Picker Widget
+
+   Output :
+
+   ![Output-7!](/week-05/docs/7.jpg "Output-7")
+
+   Output Setelah di klik :
+
+   ![Output-8!](/week-05/docs/8.jpg "Output-8")
--- week-05/src/lib/main.dart
+++ week-05/src/lib/main.dart
@@ -11,43 +11,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          primarySwatch: Colors.blue,
+        ),
+        // home: const MyHomePage(title: 'Flutter Demo'),
+        // home: const Scaffold(
+        //   body: MyLayout(),
+        // )
+        // home: Scaffold(
+        //     appBar: AppBar(
+        //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        //       title: const Text("Contoh Text Field"),
+        //     ),
+        //     body: const TextField(
+        //       obscureText: false,
+        //       decoration: InputDecoration(
+        //         border: OutlineInputBorder(),
+        //         labelText: 'Nama',
+        //       ),
+        //     )),
+        home: const MyHomePage(title: "Contoh Date Picker"));
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,69 +46,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           const MyTextWidget(),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headlineMedium,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     bottomNavigationBar: BottomAppBar(
+  //         child: Container(
+  //       height: 50.0,
+  //     )),
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: _incrementCounter,
+  //       tooltip: 'Increment',
+  //       child: const Icon(Icons.add),
+  //     ),
+  //     floatingActionButtonLocation: FloatingActionButtonLocation
+  //         .centerDocked, // This trailing comma makes auto-formatting nicer for build methods.
+  //   );
+  // }
+  DateTime selectedDate = DateTime.now();
+
+  // Initial SelectDate FLutter
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(1990),
+        lastDate: DateTime(2025));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const MyImageWidget(),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Text("${selectedDate.toLocal()}".split(' ')[0]),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: const Text('Pilih Tanggal'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
@@ -141,4 +145,40 @@ class MyImageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Image(image: AssetImage("assets/logo_polinema.png"));
   }
+}
+
+class MyLayout extends StatelessWidget {
+  const MyLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          showAlertDialog(context);
+        },
+        child: const Text("Show Alert Dialog"),
+      ),
+    );
+  }
+}
+
+showAlertDialog(BuildContext context) {
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  AlertDialog alert = AlertDialog(
+    title: const Text("My Alert Dialog"),
+    content: const Text("This is an alert message."),
+    actions: [
+      okButton,
+    ],
+  );
+
+  showDialog(context: context, builder: (BuildContext context) => alert);
 }
