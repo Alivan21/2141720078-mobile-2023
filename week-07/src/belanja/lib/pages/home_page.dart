96a474160876dcbf5f905d4ae8c05b0f00ee74df
alfanalivan <alfanalivan@gmail.com>
2023-11-06 05:54:27 +0700

    Week_07: Widget Decomposition

--- week-07/src/belanja/lib/pages/home_page.dart
+++ week-07/src/belanja/lib/pages/home_page.dart
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
         name: 'Steak',
         price: 4000,
         photoUrl:
-            'https://images.unsplash.com/photo-1600891964092-4316c288032e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
+            'https://images.unsplash.com/photo-1600891964092-4316c288032e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1p;&w=2070&q=80',
         stock: 2,
         rating: 4.5),
   ];
@@ -40,45 +40,59 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             const Text(
-              'Nama: Alfan Olivan\nNim : 2141720078', // Your desired text here
+              'Nama: Alfan Olivan\nNim: 2141720078',
               style: TextStyle(
-                fontSize: 20, // Adjust the font size as needed
-                fontWeight: FontWeight.bold, // Add styling as needed
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(
-                height: 16), // Add some spacing between the text and ListView
+            const SizedBox(height: 16),
             Expanded(
               child: ListView.builder(
                 padding: const EdgeInsets.all(8),
                 itemCount: items.length,
                 itemBuilder: (context, index) {
                   final item = items[index];
-                  return InkWell(
-                    onTap: () =>
-                        Navigator.pushNamed(context, '/item', arguments: item),
-                    child: Card(
-                      child: Container(
-                        margin: const EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            Expanded(child: Text(item.name)),
-                            Expanded(
-                              child: Text(
-                                'Rp ${item.price.toString()}',
-                                textAlign: TextAlign.end,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
+                  return ItemCard(
+                      item: item,
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/item', arguments: item);
+                      });
                 },
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+  final VoidCallback onPressed;
+
+  const ItemCard({super.key, required this.item, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Card(
+        child: Container(
+          margin: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Expanded(child: Text(item.name)),
+              Expanded(
+                child: Text(
+                  'Rp ${item.price.toString()}',
+                  textAlign: TextAlign.end,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- week-07/src/belanja/lib/pages/item_page.dart
+++ week-07/src/belanja/lib/pages/item_page.dart
@@ -22,48 +22,69 @@ class ItemPage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 0.7,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: itemArgs.stock,
-        itemBuilder: (BuildContext context, int index) {
-          final item = itemArgs;
-          return Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Image.network(
-                    item.photoUrl,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Stock: ${item.stock}',
-                  style: const TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 5),
-                Row(
-                  children: [
-                    const Icon(Icons.star, color: Colors.yellow),
-                    const SizedBox(width: 5),
-                    Text(
-                      '${item.rating}',
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-              ],
+      body: ItemGrid(itemArgs: itemArgs),
+    );
+  }
+}
+
+class ItemGrid extends StatelessWidget {
+  final Item itemArgs;
+
+  const ItemGrid({super.key, required this.itemArgs});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.7,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: itemArgs.stock,
+      itemBuilder: (BuildContext context, int index) {
+        return ItemCard(item: itemArgs);
+      },
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Item item;
+
+  const ItemCard({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Image.network(
+              item.photoUrl,
+              fit: BoxFit.cover,
             ),
-          );
-        },
+          ),
+          const SizedBox(height: 10),
+          Text(
+            'Stock: ${item.stock}',
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Icon(Icons.star, color: Colors.yellow),
+              const SizedBox(width: 5),
+              Text(
+                '${item.rating}',
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
