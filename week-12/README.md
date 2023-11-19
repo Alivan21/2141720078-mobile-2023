24c4ed53e7bf775a784b5bf9a979958b77800f07
alfanalivan <alfanalivan@gmail.com>
2023-11-19 17:04:56 +0700

    W12: soal 4 - 7

--- week-12/README.md
+++ week-12/README.md
@@ -17,6 +17,8 @@ Tugas : Minggu 12
 - [Repository Mobile Programming Class 2023.](#repository-mobile-programming-class-2023)
   - [Table of Contents](#table-of-contents)
   - [Praktikum 1](#praktikum-1)
+  - [Pratiukum 2](#pratiukum-2)
+  - [Praktikum 3](#praktikum-3)
 
 ### Praktikum 1
 
@@ -110,6 +112,52 @@ class _FuturePageState extends State<FuturePage> {
 
 Maksud dari kode tersebut adalah untuk menampilkan data dari API Google Books. Substring digunakan untuk membatasi jumlah karakter yang ditampilkan. Sedangkan catchError digunakan untuk menampilkan pesan error jika terjadi error.
 
-4. Demo Aplikasi
+Demo Aplikasi
 
 ![Gambar 2](/week-12/docs/soal1.3.gif)
+
+### Pratiukum 2
+
+4. Jelaskan maksud kode langkah 1 dan 2 tersebut!
+
+```dart
+Future<int> returnOneAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 1;
+}
+
+Future<int> returnTwoAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 2;
+}
+
+Future<int> returnThreeAsync() async {
+  await Future.delayed(const Duration(seconds: 3));
+  return 3;
+}
+```
+
+Kode di atas adalah contoh implementasi fungsi-fungsi asynchronous dalam Dart menggunakan Future. Setiap fungsi (returnOneAsync, returnTwoAsync, dan returnThreeAsync) mengembalikan nilai integer setelah mengalami penundaan selama 3 detik menggunakan Future.delayed. Masing-masing fungsi mengembalikan nilai yang berbeda (1, 2, dan 3). Dengan kata lain, ketika fungsi-fungsi ini dipanggil, mereka akan mengembalikan nilai setelah waktu tertentu tanpa menghentikan eksekusi program secara keseluruhan, karena mereka bekerja secara asynchronous.
+
+Demo Aplikasi
+
+![Gambar 3](/week-12/docs/soal2.4.gif)
+
+### Praktikum 3
+
+5. Jelaskan maksud kode langkah 2 tersebut!
+
+- completer dideklarasikan sebagai objek Completer, yang bertanggung jawab untuk menangani penyelesaian (completion) dari suatu Future.
+- Fungsi getNumber menginisialisasi completer dan kemudian memanggil fungsi calculate(). Fungsi ini mengembalikan completer.future, yang merupakan objek Future yang akan diselesaikan di masa depan.
+- Fungsi calculate adalah fungsi asynchronous yang melakukan penundaan selama 5 detik menggunakan Future.delayed dan kemudian menyelesaikan completer dengan nilai 42.
+
+Demo Aplikasi
+
+![Gambar 4](/week-12/docs/soal3.5.gif)
+
+6. Jelaskan maksud langkah 2 dengan langkah 5-6 tersebut!
+
+Kode di atas merupakan modifikasi dari kode sebelumnya dengan penambahan penanganan error. Fungsi calculate kini berada dalam blok try-catch, di mana jika terjadi kesalahan (error), completer akan diselesaikan dengan error tersebut. Pada bagian onPressed, hasil dari fungsi getNumber() di-handle menggunakan then untuk menangani hasil sukses dan catchError untuk menangani kesalahan. Jika berhasil, nilai result diupdate dengan nilai yang diterima, dan jika terjadi kesalahan, nilai result diatur sebagai 'An error occurred'.
+
+Demo Aplikasi
+![Gambar 5](/week-12/docs/soal3.5.gif)
--- week-12/src/books/lib/main.dart
+++ week-12/src/books/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:async/async.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
@@ -34,6 +35,7 @@ class FuturePage extends StatefulWidget {
 class _FuturePageState extends State<FuturePage> {
   String result = '';
   bool isLoading = false;
+  late Completer completer;
 
   @override
   Widget build(BuildContext context) {
@@ -42,31 +44,24 @@ class _FuturePageState extends State<FuturePage> {
         title: const Text('Back from the Future'),
       ),
       body: Center(
-        child: Column(children: [
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
           const Spacer(),
           ElevatedButton(
             child: const Text('GO!'),
             onPressed: () {
-              setState(() {
-                isLoading = true;
-              });
-              Future.delayed(const Duration(seconds: 1)).then(((value) {
-                getData().then((value) {
-                  result = value.body.toString().substring(0, 450);
-                  setState(() {
-                    isLoading = false;
-                  });
-                }).catchError((e) {
-                  result = "An error occured $e";
-                  setState(() {
-                    isLoading = false;
-                  });
+              getNumber().then((value) {
+                setState(() {
+                  result = value.toString();
                 });
-              }));
+              }).catchError((e) {
+                result = 'An error occurred';
+              });
             },
           ),
           const Spacer(),
-          isLoading ? CircularProgressIndicator() : Text(result),
+          Text('Result: $result'),
+          const Spacer(),
+          const CircularProgressIndicator(),
           const Spacer(),
         ]),
       ),
@@ -78,5 +73,68 @@ class _FuturePageState extends State<FuturePage> {
     const path = '/books/v1/volumes/_i6bDeoCQzsC';
     Uri url = Uri.https(authority, path);
     return await http.get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+
+    setState(() {
+      result = total.toString();
+    });
+  }
+
+  Future getNumber() {
+    completer = Completer<int>();
+    calculate();
+    return completer.future;
+  }
+
+  Future calculate() async {
+    try {
+      await Future.delayed(const Duration(seconds: 5));
+      completer.complete(42);
+    } catch (e) {
+      completer.completeError(e);
+    }
+  }
+
+  void returnFG() {
+    FutureGroup<int> futureGroup = FutureGroup<int>();
+    futureGroup.add(returnOneAsync());
+    futureGroup.add(returnTwoAsync());
+    futureGroup.add(returnThreeAsync());
+    futureGroup.close();
+
+    futureGroup.future.then((List<int> value) {
+      int total = 0;
+      for (var element in value) {
+        total += element;
+      }
+      setState(() {
+        result = total.toString();
+      });
+    }).catchError((e) {
+      setState(() {
+        result = 'An error occurred';
+      });
+    });
   }
 }
