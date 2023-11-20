b87b1ca0dc344894b93bc0c010b2dac491409d8a
alfanalivan <alfanalivan@gmail.com>
2023-11-20 09:18:47 +0700

    W13: Jawaban Soal 2

---
+++ week-13/src/stream_alfan/lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.yellow,
+    Colors.purple,
+  ];
+}
